6f1f804f0409a6a4b315e87301c0bb82144cfd75
ljubicjanko1@gmail.com <ljubicjanko1@gmail.com>
2024-11-13 13:06:10 +0100

    header

--- src/main/java/cbd/order_tracker/model/LoginResponse.java
+++ src/main/java/cbd/order_tracker/model/LoginResponse.java
@@ -5,72 +5,82 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LoginResponse {
-    private Integer id;
-    private String token;
-    private long expiresIn;
-    private Set<String> roles;
-    private Set<String> privileges;
+	private Integer id;
+	private String token;
+	private long expiresIn;
+	private Set<String> roles;
+	private Set<String> privileges;
+	private String name;
 
-    public String getToken() {
-        return token;
-    }
+	public String getToken() {
+		return token;
+	}
 
-    public LoginResponse setToken(String token) {
-        this.token = token;
-        return this;
-    }
+	public LoginResponse setToken(String token) {
+		this.token = token;
+		return this;
+	}
 
-    public LoginResponse() {
-    }
+	public LoginResponse() {
+	}
 
-    public LoginResponse(Integer id, String token, long expiresIn, Collection<Role> roles) {
-        this.id = id;
-        this.token = token;
-        this.expiresIn = expiresIn;
-        this.roles = roles.stream()
-                .map(Role::getName)
-                .collect(Collectors.toSet());
-        this.privileges = roles.stream()
-                .flatMap(role -> role.getPrivileges().stream())
-                .map(Privilege::getName)
-                .collect(Collectors.toSet());
-    }
+	public LoginResponse(Integer id, String token, long expiresIn, Collection<Role> roles, String fullName) {
+		this.id = id;
+		this.token = token;
+		this.expiresIn = expiresIn;
+		this.roles = roles.stream()
+				.map(Role::getName)
+				.collect(Collectors.toSet());
+		this.privileges = roles.stream()
+				.flatMap(role -> role.getPrivileges().stream())
+				.map(Privilege::getName)
+				.collect(Collectors.toSet());
+		this.name = fullName;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public long getExpiresIn() {
-        return expiresIn;
-    }
+	public long getExpiresIn() {
+		return expiresIn;
+	}
 
-    public LoginResponse setExpiresIn(long expiresIn) {
-        this.expiresIn = expiresIn;
-        return this;
-    }
+	public LoginResponse setExpiresIn(long expiresIn) {
+		this.expiresIn = expiresIn;
+		return this;
+	}
 
-    public Set<String> getRoles() {
-        return roles;
-    }
+	public Set<String> getRoles() {
+		return roles;
+	}
 
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
+	}
 
-    public Set<String> getPrivileges() {
-        return privileges;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setPrivileges(Set<String> privileges) {
-        this.privileges = privileges;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public String toString() {
-        return "LoginResponse{" + "token='" + token + '\'' + ", expiresIn=" + expiresIn + '}';
-    }
+	public Set<String> getPrivileges() {
+		return privileges;
+	}
+
+	public void setPrivileges(Set<String> privileges) {
+		this.privileges = privileges;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginResponse{" + "token='" + token + '\'' + ", expiresIn=" + expiresIn + '}';
+	}
 }
